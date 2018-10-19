26b0b0a4f88d88a8aeb4b7ceca9c497e74bbb301
BrisiaCastrejon <brissmonse@gmail.com>
2018-10-19 15:36:15 -0500

    Cambio de class a ClassName

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -10,13 +10,13 @@ class Register extends Component {
     return (
       <div className="container form-centrado">
         <form className="row justify-content-center fondo-container">
-          <div class="form-group primera-caja">
+          <div className="form-group primera-caja">
           <img className=""  src={image} alt="" width="300px"/>
-          <input type="name" class="form-control" placeholder="Ingresa tu nombre"/>
-          <input type="email" class="form-control" placeholder="Ingresa un correo"/>
-          <input type="password" class="form-control" placeholder="Ingresa una contraseña"/>
+          <input type="name" className="form-control" placeholder="Ingresa tu nombre"/>
+          <input type="email" className="form-control" placeholder="Ingresa un correo"/>
+          <input type="password" className="form-control" placeholder="Ingresa una contraseña"/>
           <br/>
-          <button type="submit" class="btn btn-primary btn-block">Registrar</button>
+          <button type="submit" className="btn btn-primary btn-block">Registrar</button>
           </div>            
         </form>   
       </div>
